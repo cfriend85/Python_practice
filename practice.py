8fbfcbed65c3c227e79916f9e93efa10013367ad
Charles <c.friend85@yahoo.com>
2021-08-04 15:24:03 -0600

    Algos

--- practice.py
+++ practice.py
@@ -10,8 +10,25 @@ def check(n):
         if n % 2 == 0:
             print ("Weird")
 
+
 def simpleArraySum(ar):
     sum = 0
     for i in range (0, len(ar)):
         sum += ar[i]
     return sum
+
+print (simpleArraySum([1,2,3,4,5]))
+
+
+
+def compareTriplets(a, b):
+    alice = 0
+    bob = 0
+    for i in range (0, len(b)):
+        if a[i] > b[i]:
+            alice += 1
+        elif b[i] > a[i]:
+            bob += 1
+        elif a[i] == b[i]:
+            print("Tie!")
+    return [alice, bob]
