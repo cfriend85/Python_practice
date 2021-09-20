2dd9e614870648aa0bb7ceb823af1c3623c24bf7
Charles <c.friend85@yahoo.com>
2021-09-19 21:34:56 -0600

    Python practice

--- general_practice.py
+++ general_practice.py
@@ -19,10 +19,24 @@ print (str)
 
 
 # List comprehension
-#You are given three integers x,y and z representing the dimensions of a cuboid along with
+# You are given three integers x,y and z representing the dimensions of a cuboid along with
 # an integer n. Print a list of all possible coordinates given by(i,j,k) on a 3D grid where
 # the sum of i+j+k is not equal to n.
 
 print([[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if ((i+j+k) != n)])
+
+
+
+def print_full_name(first, last):
+    print(f"Hello {first} {last}! You just delved into python.")
+
+print_full_name("Charles", "Friend")
+
+
+
+def swap_case(s):
+    return s.swapcase()
+
+print(swap_case("HoWdY PaRtNeR!"))
 
 
